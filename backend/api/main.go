d0712d654259e37212027da5d39a63554e2f5ecb
ryoshindo <udoryo999@gmail.com>
2023-02-14 01:12:17 +0900

    feat: add api and k6 deployment configuration

--- backend/api/main.go
+++ backend/api/main.go
@@ -8,5 +8,5 @@ func main() {
 	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ready"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":18080", nil)
 }
