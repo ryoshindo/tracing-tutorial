c6091af67bd34cd16ad50b75ffa6e62cecba5d1f
ryoshindo <udoryo999@gmail.com>
2023-03-21 13:04:33 +0900

    fix: add new line

--- backend/api/main.go
+++ backend/api/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ready"))
+		w.Write([]byte("ready\n"))
 	})
 	http.ListenAndServe(":18080", nil)
 }
